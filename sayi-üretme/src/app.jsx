9b152da9a3029d01aa473c9ccecb591f3418dc46
Ali İslam <bgane8181@gmail.com>
2026-06-14 18:05:28 +0300

    sayi uretme eklendi

---
+++ sayi-üretme/src/app.jsx
@@ -0,0 +1,39 @@
+import {useState} from "react";
+import "./style.css"
+function App() {
+
+  const [minVal ,setMinVal] = useState(0);
+  const [maxVal ,setMaxVal] = useState(10);
+  const [randomNum ,setRandomNum] = useState(5);
+
+  const giveRandomNumber =() => {
+    setRandomNum(Math.floor(Math.random()*(maxVal-minVal +1) +minVal))
+  } 
+
+  return (
+        <>
+      <div className='hero'>
+      <div className="container">
+        <div className="randomNum">
+            <p>randomNumber : <span>{randomNum}</span></p>
+            <div className="numContainer">
+                <p>Min:</p>
+                <input type="number" value={minVal}
+                onChange={e => setMinVal(e.target.value)}
+                />
+            </div>
+            <div>
+                <p>Max:</p>
+                <input type="number" value={{maxVal}}
+                onChange={e => setMaxVal(e.target.value)}
+                />
+            </div>
+        </div>
+        <button onClick={giveRandomNumber}>Get Random Number</button>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App
